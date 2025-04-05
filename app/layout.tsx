82da441c804840b8d31a0e3c5380b8c93396453d
pedrohenriquealves1991 <pedrophalves@gmail.com>
2025-04-05 11:59:39 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Regulamentei',
+  description: 'Porto Alegre,RS, pedo@regulamentei.com.br',
   generator: 'v0.dev',
 }
 
