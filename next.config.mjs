49768b8be6963e73894d0a4e4e3a8fa8e0af3a45
pedrohenriquealves1991 <pedrophalves@gmail.com>
2025-04-08 21:09:26 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,16 @@
+let userConfig = undefined
+try {
+  // try to import ESM first
+  userConfig = await import('./v0-user-next.config.mjs')
+} catch (e) {
+  try {
+    // fallback to CJS import
+    userConfig = await import("./v0-user-next.config");
+  } catch (innerError) {
+    // ignore error
+  }
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -14,8 +27,23 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-  // Adicione esta nova configuração para transpilar framer-motion:
-  transpilePackages: ['framer-motion'],
 }
 
-export default nextConfig
+if (userConfig) {
+  // ESM imports will have a "default" property
+  const config = userConfig.default || userConfig
+
+  for (const key in config) {
+    if (
+      typeof nextConfig[key] === 'object' &&
+      !Array.isArray(nextConfig[key])
+    ) {
+      nextConfig[key] = {
+        ...nextConfig[key],
+        ...config[key],
+      }
+    } else {
+      nextConfig[key] = config[key]
+    }
+  }
+}
